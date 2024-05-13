0fb328956a6318018a3740c1f186b5427416637f
ayushi-khare <ayushi-khare@pluralsight.com>
2024-05-13 09:37:59 +0530

    Course details site complete

--- scripts/index.js
+++ scripts/index.js
@@ -9,8 +9,19 @@ fetch("http://localhost:8081/api/courses")
            let cell1 = row.insertCell(0);
            let cell2 = row.insertCell(1);
            let cell3 = row.insertCell(2);
+           let cell4 = row.insertCell(3);
+           
            cell1.innerHTML = data[i].dept;
            cell2.innerHTML = data[i].courseNum;
            cell3.innerHTML = data[i].courseName;
+        
+           //Creating anchor tag for 4th cell
+           //const detailsCell = row.insertCell();
+           let anchor = document.createElement("a");
+           anchor.href = `details.html?cid=${data[i].id}`;
+           anchor.text = "See details";  
+           cell4.appendChild(anchor);
+           
+
         }
      });
